49158ffd60c3dc7bfaf30d4a17d2130f33c8ecfd
Eugene Glova <eugene.glova@gmail.com>
2019-04-15 15:57:33 +0300

    Updates

---
+++ precache-manifest.36ec7b31fcbfda6fc97530ba6f2b905f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b294bc41246a8cfe1dd8",
+    "url": "/react-fresh-recipes/static/js/runtime~main.ee53ec14.js"
+  },
+  {
+    "revision": "06c66873d18dd3599316",
+    "url": "/react-fresh-recipes/static/js/main.0b68c61e.chunk.js"
+  },
+  {
+    "revision": "43eecb3243c099476764",
+    "url": "/react-fresh-recipes/static/js/2.2102cc2e.chunk.js"
+  },
+  {
+    "revision": "06c66873d18dd3599316",
+    "url": "/react-fresh-recipes/static/css/main.91f6e81f.chunk.css"
+  },
+  {
+    "revision": "b1ebe9b28911d81a85592bc96757cd6a",
+    "url": "/react-fresh-recipes/index.html"
+  }
+];
